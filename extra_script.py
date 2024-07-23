a48df93381506474a46d8d91749bc28dc576b0a2
Mykola Basov <git@mbasov.com>
2024-07-23 19:47:23 +0100

    add terje's plugin

--- extra_script.py
+++ extra_script.py
@@ -12,7 +12,7 @@ env.AddPostAction(
 def after_build(source, target, env): 
     print("[From Script] Finished building!!")
     env.Replace(PROGNAME="FLEXI_HAL_%s" % env.GetProjectOption("custom_prog_version")+"_"+env.GetProjectOption("grblhal_driver_version"))
-    env.Execute("python uf2conv.py -c -b 0x08010000 -f 0x57755a57 $BUILD_DIR/firmware.hex --output ${PROGNAME}.uf2")
+    env.Execute("python3 uf2conv.py -c -b 0x08010000 -f 0x57755a57 $BUILD_DIR/firmware.hex --output ${PROGNAME}.uf2")
 
 env.AddPostAction("buildprog", after_build)
 
